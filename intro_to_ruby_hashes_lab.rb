d5ce7c7870fd832929eb9ef08fb271942b47568c
Peter Stone <peter.stone.online@gmail.com>
2018-09-27 18:05:19 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,9 +14,7 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-  monopoly = {}
-  monopoly[:railroads] = {}
-  monopoly[:railroads][:pieces] = 4
+  monopoly = {railroads: {pieces: 4}}
   monopoly
 end
 
